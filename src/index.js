895ace321b4f98aba639d3bbc90e64e9bdf06f80
gabriel gonçalves da silva <gabriel24f90@gmail.com>
2022-09-18 20:58:39 -0300

    ksksksks

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './paginas/home';
-import LoginADM from './paginas/login'
-import CTE from './paginas/cadastrarTenis'
-import Barra from '../src/componentes/barra'
+import LoginADM from './paginas/login';
+import CTE from './paginas/cadastrarTenis';
+import LUS from './paginas/loginUsuario';
+import Barra from '../src/componentes/barra';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CTE/>
+    <LUS/>
   </React.StrictMode>
 );
 
