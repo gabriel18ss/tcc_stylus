64382ac22f02c3a40852111f118b9b7b9abb8324
junior batista <juniorlevin878@gmail.com>
2022-09-19 11:02:33 -0300

    alt

--- src/componentes/barra/index.js
+++ src/componentes/barra/index.js
@@ -1,11 +1,17 @@
-
+import './index.scss';
 
 
 export default function Barra(props){
     return(
         <section className='comp-barra'>
-            <div className="barra">
-
+            <div className='barra'>
+                <div>
+                    <img className='logo' src="/images/logo.jpeg" alt=""/>
+                </div>
+                <div>
+                  <input className='barra-pesquisa'/>
+                </div>
+             
             </div>
         </section>
     )
