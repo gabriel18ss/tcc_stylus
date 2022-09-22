34f211430a16f37f4d1f7e23f7b7fa1339afd5c6
Junior Batista <juniorlevin878@gmail.com>
2022-09-22 18:03:24 -0300

    api cadastro quase

--- site/src/paginas/cadastrarTenis/index.js
+++ site/src/paginas/cadastrarTenis/index.js
@@ -1,5 +1,8 @@
 import './index.scss';
 import Barra from '../../componentes/barra';
+import storage from 'local-storage';
+
+import { cadastrarTenis } from '../../api/produtoApi.js';
 
 import { useState } from 'react';
 
@@ -12,9 +15,19 @@ export default function CTE (){
     const [nome, setNome] = useState ('')
     const [quantidade, setQuantidade] = useState ('')
     const [valor, setValor] = useState ('')
-    const [lancamento, setlancamento] = useState ('')
+    const [lancamento, setlancamento] = useState (false)
     const [tamanho, setTamanho] = useState ('')
     
+    async function salvarClick(){
+        try{
+            const r = await cadastrarTenis(marca, genero, nome, quantidade, valor, tamanho);
+            alert('pedido cadastrado uppppp');
+            
+        }catch (err){
+            alert(err.message);
+        }
+
+    }
 
     return(
         <section>
@@ -43,29 +56,35 @@ export default function CTE (){
                     <input className='input2'></input>
 
                         <h4>Quantidade</h4>
-                        <input className='input3' type='text' value={quantidade}></input>
+                        <input className='input3' type='text' value={quantidade} onChange={e => setQuantidade(e.target.value)}></input>
 
                         <h4>Genero</h4>
-                        <input className='input3'></input>
+                        <input className='input3' type='text' value={genero} onChange={e => setGenero(e.target.value)}></input>
+
+                        <h4 className='titulo'>lançamento</h4>
+                        <input type='checkbox'  checked={lancamento} onChange ={e => setlancamento(e.target.value)}></input>
 
                     </div>
                     <div>
                         <h4 className='titulo'>Nome</h4>
-                        <input className='input4'></input>
+                        <input className='input4' type='text' value={nome} onChange={e => setNome(e.target.value)}></input>
 
                         <h4 className='titulo'>Numero</h4>
-                        <input className='input4'></input>
+                        <input className='input4' type='number' value={tamanho} onChange={e => setTamanho(e.target.value)}></input>
 
                         <h4 className='titulo'>Marca</h4>
-                        <input className='input4'></input>
+                        <input className='input4' type='text' value={marca} onChange={e => setMarca(e.target.value)}></input>
 
                         <h4 className='titulo'>preço</h4>
-                        <input className='input4'></input>
+                        <input className='input4' type='text' value={valor} onChange={e => setValor(e.target.value)}></input>
+
+                        
+
 
                     </div>
                
              </div>
-                    <button className='botaoF'>Finalizar</button>
+                    <button onClick={salvarClick} className='botaoF'>Finalizar</button>
             </div>
             </main>            
         </section>
