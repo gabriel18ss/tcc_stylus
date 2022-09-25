ad50f3586f9f83670d4bc60ca0750beaaa372e76
gabriel gonçalves da silva <gabriel24f90@gmail.com>
2022-09-25 13:08:44 -0300

    pagina login 85%

--- site/src/paginas/loginUsuario/index.js
+++ site/src/paginas/loginUsuario/index.js
@@ -1,9 +1,33 @@
 import './index.scss';
+import Barra from '../../componentes/barra'
 
 export default function UsuarioLogin(){
     return(
         <section>
-            <h1>pagina login</h1>
+            <div>
+                <Barra/>
+            </div>
+            <div>
+                <img className="imagem" src="/images/tenisLog.png" alt="" />
+                <div className='login'>
+                    
+                    <h1 className='titulo'>iniciar sessão</h1>
+
+                    <div className='dig'>
+                    <input type='text' placeholder='E-Mail:' className='e-mail'/>
+                    
+                    <input type='text' placeholder='Senha:' className='e-mail'/>
+                    
+                    
+                    </div>
+
+                    <button className="button">ENTRAR</button>
+
+                    <h1 className="reg">não esta registrado ?</h1>
+                    <h1 className="junt">junte-se a nos</h1>
+                    
+                </div>
+            </div>
         </section>
 
     )
