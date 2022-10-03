39e052a37c945b39d7df696e2c030df466f26bf0
gabriel <gabriel24f90@gmail.com>
2022-10-03 11:51:11 -0300

    alt vitrine

---
+++ site/src/paginas/vitrine/index.js
@@ -0,0 +1,24 @@
+import './index.scss';
+import Barra from '../../componentes/barra'
+export default function vitrine(){
+    return(
+   
+
+        <section>
+         <div>
+            Barra
+        </div>
+            <div>
+                <h1>marcas</h1>
+
+                <b>adidas</b>
+                <b>adidas</b>
+                <b>adidas</b>
+                <b>adidas</b>
+
+                <input/>
+                <label placeholder=''/>
+            </div>
+        </section>
+    )
+}
